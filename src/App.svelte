6c53086352274dbe3e1d51dbb080c5de47d5b5a8
Yusuke Kido <mail@yusuk.eki.do>
2022-03-09 18:19:18 +0900

    Scaffold and format

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+  main {
+      margin: 0 auto;
+      padding: 1em;
+      max-width: 240px;
+      text-align: center;
+    }
+
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-weight: 100;
+      font-size: 4em;
+    }
+
+    @media (min-width: 640px) {
+      main {
+        max-width: none;
+      }
+    }
+</style>
